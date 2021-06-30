16c5fef414382a3a28e9d25534a050c30cf23f55
AzureAD\RenugadeviSadagoban <renugadevi.sadagoban@syncfusion.com>
2021-06-30 12:32:05 +0530

    Sample changes has added

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _ChangeColumnBackgroundColorSampleState
         source: employeeDataSource,
         columnWidthMode: ColumnWidthMode.fill,
         columns: <GridColumn>[
-          GridTextColumn(
+          GridColumn(
               columnName: 'id',
               label: Container(
                   padding: EdgeInsets.all(16.0),
@@ -57,13 +57,13 @@ class _ChangeColumnBackgroundColorSampleState
                   child: Text(
                     'ID',
                   ))),
-          GridTextColumn(
+          GridColumn(
               columnName: 'name',
               label: Container(
                   padding: EdgeInsets.all(8.0),
                   alignment: Alignment.center,
                   child: Text('Name'))),
-          GridTextColumn(
+          GridColumn(
               columnName: 'designation',
               label: Container(
                   padding: EdgeInsets.all(8.0),
@@ -72,7 +72,7 @@ class _ChangeColumnBackgroundColorSampleState
                     'Designation',
                     overflow: TextOverflow.ellipsis,
                   ))),
-          GridTextColumn(
+          GridColumn(
               columnName: 'salary',
               label: Container(
                   padding: EdgeInsets.all(8.0),
